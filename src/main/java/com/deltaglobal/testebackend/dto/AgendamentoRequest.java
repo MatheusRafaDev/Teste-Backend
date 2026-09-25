2a7e8df1ffedf997ab5774e8f1b2bacdb8e05819
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2026-09-25 01:16:59 -0300

    fix: remove unused validation imports dos DTOs

--- src/main/java/com/deltaglobal/testebackend/dto/AgendamentoRequest.java
+++ src/main/java/com/deltaglobal/testebackend/dto/AgendamentoRequest.java
@@ -1,6 +1,5 @@
 package com.deltaglobal.testebackend.dto;
 
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
--- src/main/java/com/deltaglobal/testebackend/dto/DepositoRequest.java
+++ src/main/java/com/deltaglobal/testebackend/dto/DepositoRequest.java
@@ -1,7 +1,5 @@
 package com.deltaglobal.testebackend.dto;
 
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import io.swagger.v3.oas.annotations.media.Schema;
